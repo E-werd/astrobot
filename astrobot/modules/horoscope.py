b38cf44470e1968c345a7361592c90c95c337cc5
E-werd <1765057+E-werd@users.noreply.github.com>
2023-08-24 14:54:12 -0400

    Refactor data check and associated functions

--- astrobot/modules/horoscope.py
+++ astrobot/modules/horoscope.py
@@ -39,11 +39,10 @@ class Horoscope:
         :data: Dict to check and manipulate'''
         d: dict = data
         if (d == {}):
-            logging.debug("Data is empty. Getting data...")
+            logging.debug("Data is empty. Creating structure...")
             d.update(self.__create_structure())
-            d = self.update_all(data=d)
-        else:
-            d = self.__check_data(data=d)
+        
+        d = self.__check_data(data=d)
         logging.debug(f"Data should be ready.")
         return d
         
@@ -52,7 +51,6 @@ class Horoscope:
         :horo: Horo object'''
         logging.debug(f"Fetching horoscope: {horo.zodiac.name}, {horo.date}, {horo.style.name} from {horo.source.name}")
         
-        Day.update_days()
         day = self.get_day(date=horo.date)
 
         match horo.source:
@@ -65,29 +63,30 @@ class Horoscope:
             case _: return "", "Unknown Source" # This should never happen. Update loop with new sources.    
     
     def __move_data_day(self, 
-                        start: Day.Type, 
-                        dest: Day.Type, 
+                        start: Day.Type,
+                        dest: Day.Type,
+                        source: Source.Type,
+                        style: Style.Type,
                         data: dict) -> dict:
         '''Moves data in buffer, used in update operations. Returns dict
         :start: source day
         :dest: destination day
         :source: source to move
+        :style: style to move
         :data: Dict to manipulate'''
         d: dict = data
 
-        for source in Source.types:
-            for style in Source.types[source].styles:
-                work: dict = d["horoscopes"]["sources"][Source.types[source].name]["styles"][style.name]["days"]
-                
-                logging.debug(f"Moving data from '{start.name}' to '{dest.name}' for style '{style.full}' from source '{Source.types[source].full}'...") 
+        work: dict = d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"]
+        
+        logging.debug(f"Moving data from '{start.name}' to '{dest.name}' for style '{style.full}' from source '{source.full}'...") 
 
-                start_date: str = work[start.name]["date"]
-                start_signs: dict = work[start.name]["signs"]
+        start_date: str = work[start.name]["date"]
+        start_signs: dict = work[start.name]["signs"]
 
-                work[dest.name].update({"date": start_date})
-                work[dest.name]["signs"].update(start_signs)
+        work[dest.name].update({"date": start_date})
+        work[dest.name]["signs"].update(start_signs)
 
-                d["horoscopes"]["sources"][Source.types[source].name]["styles"][style.name]["days"].update(work)
+        d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"].update(work)
 
         return d
 
@@ -96,65 +95,65 @@ class Horoscope:
         :data: Dict to check and manipulate'''
         data_in: dict = data
         Day.update_days()
-        d: dict = data_in["horoscopes"]["sources"][Source.astrology_com.name]["styles"][Style.daily.name]["days"][Day.tomorrow.name]
 
-        d_date: datetime = datetime.strptime(d["date"], "%B %d, %Y")
-        d_date_str: str = d_date.strftime('%B %d, %Y')
-        t_date: datetime = Day.tomorrow.date
-        t_date_str: str = t_date.strftime('%B %d, %Y')
+        for _, source in Source.types.items():
+            for style in source.styles:
+                logging.debug(f"Checking local data from {source.full} for {style.full}...")
+                # Check what data thinks is tomorrow against reality
+                d = data_in["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][Day.tomorrow.name]["date"]
+                d_date: datetime = datetime.strptime(str(d), "%B %d, %Y")           # Data date
+                t_date: datetime = datetime.strptime(Day.tomorrow.ymd, "%B %d, %Y") # Tomorrow date
+                logging.debug(f"Comparing local data's tomorrow date ({d_date}) to tomorrow's real date ({t_date})...")
+                if (d_date == t_date):
+                    logging.info("Data is current.")
+                    continue
+                
+                # Check what data thinks is tomorrow against what the source thinks is tomorrow
+                logging.debug("Mismatch. Retrieving date from source...")
+                h = Horo(zodiac=Zodiac.aries, date=Day.tomorrow.ymd, style=style, source=source)
+                date, _ = self.__fetch(horo=h)
+                s_date = datetime.strptime(date, "%B %d, %Y") # Source date
+                logging.debug(f"Comparing source data's tomorrow date ({s_date}) to local data's tomorrow date ({d_date})...")
+                if (s_date == d_date):
+                    logging.info("Data is current.") 
+                    continue
 
-        logging.info("Checking data...")
-
-        logging.debug(f"Comparing local data's tomorrow date ({d_date_str}) to tomorrow's real date ({t_date_str})...")
-        if (d_date_str == t_date_str): 
-            logging.info("Data is current.")
-            return data_in
-        logging.debug("Mismatch. Retrieving date from source...")
-
-        s: AstrologyCom = AstrologyCom(zodiac=Zodiac.aries, day=Day.tomorrow, style=Style.daily)
-        s_date: datetime = datetime.strptime(s.date, "%B %d, %Y")
-        s_date_str: str = s_date.strftime('%B %d, %Y')
-
-        logging.debug(f"Comparing source data's tomorrow date ({s_date_str}) to local data's tomorrow date ({d_date_str})...")
-        if (s_date_str == d_date_str): 
-            # Do nothing
-            logging.info("Data is current.") 
-            return data_in
-        
-        d_datep1: datetime = d_date + timedelta(days=1)
-        d_datep1_str: str = d_datep1.strftime('%B %d, %Y')
-
-        logging.debug(f"Comparing source data's tomorrow date ({s_date_str}) to the day after local data's tomorrow date ({d_datep1_str})...")
-        if (s_date_str == d_datep1_str):
-            # One day behind, move: today->yesterday, tomorrow->today
-            logging.info("Data one day behind.") 
-            data_in = self.__move_data_day(start=Day.today, dest=Day.yesterday, data=data_in) # move today to yesterday
-            data_in = self.__move_data_day(start=Day.tomorrow, dest=Day.today, data=data_in) # move tomorrow to today
+                # Check if we're out by 1 day
+                d_datep1: datetime = d_date + timedelta(days=1) # Data date, plus 1 day
+                logging.debug(f"Comparing source data's tomorrow date ({s_date}) to the day after local data's tomorrow date ({d_datep1})...")
+                if (s_date == d_datep1):
+                    # One day behind, move: today->yesterday, tomorrow->today
+                    logging.info("Data one day behind.") 
+                    data_in = self.__move_data_day(start=Day.today, dest=Day.yesterday, source=source, style=style, data=data_in) # move today to yesterday
+                    data_in = self.__move_data_day(start=Day.tomorrow, dest=Day.today, source=source, style=style, data=data_in) # move tomorrow to today
+                    
+                    # Update: tomorrow
+                    data_in = self.__update_day(day=Day.tomorrow, source=source, style=style, data=data_in)
+                
+                # Check if we're out by 2 days
+                d_datep2: datetime = d_date + timedelta(days=2) # Data date, plus 2 days
+                logging.debug(f"Comparing source data's tomorrow date ({s_date}) to two days after local data's tomorrow date ({d_datep2})...")
+                if (s_date == d_datep2): 
+                    # Two days behind, move: tomorrow->yesterday
+                    logging.info("Data two days behind.") 
+                    data_in = self.__move_data_day(start=Day.tomorrow, dest=Day.yesterday, source=source, style=style, data=data_in) # move tomorrow to yesterday
+                    
+                    # Update: tomorrow + today
+                    data_in = self.__update_day(day=Day.tomorrow, source=source, style=style, data=data_in)
+                    data_in = self.__update_day(day=Day.today, source=source, style=style, data=data_in)
+                
+                # It must all be out of date, update all
+                logging.info("All data out of date.")
+                data_in = self.__update_day(day=Day.tomorrow, source=source, style=style, data=data_in)
+                data_in = self.__update_day(day=Day.today, source=source, style=style, data=data_in)
+                data_in = self.__update_day(day=Day.yesterday, source=source, style=style, data=data_in)
             
-            # Update: tomorrow
-            data_in = self.__update_day(day=Day.tomorrow, data=data_in)
-            return data_in
-        
-        d_datep2: datetime = d_date + timedelta(days=2)
-        d_datep2_str: str = d_datep2.strftime('%B %d, %Y')
-
-        logging.debug(f"Comparing source data's tomorrow date ({s_date_str}) to two days after local data's tomorrow date ({d_datep2_str})...")
-        if (s_date_str == d_datep2_str): 
-            # Two days behind, move: tomorrow->yesterday
-            logging.info("Data two days behind.") 
-            data_in = self.__move_data_day(start=Day.tomorrow, dest=Day.yesterday, data=data_in) # move tomorrow to yesterday
-            
-            # Update: tomorrow + today
-            data_in = self.__update_day(day=Day.tomorrow, data=data_in)
-            data_in = self.__update_day(day=Day.today, data=data_in)
-            return data_in
-        
-        # All out of date, update all
-        logging.info("All data out of date.") 
-        data_in = self.update_all(data=data_in)
         return data_in
 
-    def __update_day(self, day: Day.Type, data: dict) -> dict:
+    def __update_day(self, day: Day.Type,
+                     source: Source.Type,
+                     style: Style.Type,
+                     data: dict) -> dict:
         '''Updates data for a specific day for all styles. Returns dict
         :day: day to update
         :source: source to update from
@@ -162,20 +161,18 @@ class Horoscope:
         d: dict = data
         logging.info(f"Updating all data for {day.full}...")
 
-        for _, source in Source.types.items():
-            for style in source.styles:
-                add = {day.name: {"date": "", "emoji": day.symbol, "signs": {}}}
-                d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"].update(add)
-                for _, zodiac in Zodiac.types.items():
-                    h = Horo(zodiac=zodiac,
-                                date=day.ymd,
-                                style=style,
-                                source=source)
-                    date, text = self.__fetch(horo=h)
-                    add = {zodiac.name: text}
-                    d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name]["signs"].update(add)
-                    add = {"date": date}
-                    d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name].update(add)
+        add = {day.name: {"date": "", "emoji": day.symbol, "signs": {}}}
+        d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"].update(add)
+        for _, zodiac in Zodiac.types.items():
+            h = Horo(zodiac=zodiac,
+                        date=day.ymd,
+                        style=style,
+                        source=source)
+            date, text = self.__fetch(horo=h)
+            add = {zodiac.name: text}
+            d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name]["signs"].update(add)
+            add = {"date": date}
+            d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name].update(add)
 
         return d
 
@@ -218,17 +215,6 @@ class Horoscope:
         text: str = d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name]["signs"][zodiac.name]
         date: str = d["horoscopes"]["sources"][source.name]["styles"][style.name]["days"][day.name]["date"]
         return Horo(zodiac=zodiac, date=date, source=source, style=style, text=text)
-
-    def update_all(self, data: dict) -> dict:
-        '''Updates all data. Returns dict
-        :data: Dict to check and manipulate'''
-        d: dict = data
-        logging.debug("Updating all data from sources...")
-        
-        for day in Day.types:
-            d = self.__update_day(day=Day.types[day], data=d)
-
-        return d
 
     def check_updates(self, data: dict) -> dict:
         '''Check for updates, wraps __check_data(). Returns dict
